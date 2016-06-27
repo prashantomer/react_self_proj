b49aa1f1c7fbc27e575e45dcd9613f2997a42b3e
Hitesh Kanwar <hitesh.kanwar786@gmail.com>
2016-06-27 19:07:03 +0530

    Feature # Changing loader babel to babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ var config = {
        {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
     
         query: {
           presets: ['es2015', 'react']
